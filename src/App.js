d7b371d005cf7b317b293efa6581d859dbdbda8d
Saubhik Mukherjee <saubhik.mukherjee@gmail.com>
2018-10-08 07:58:26 +0530

    Created ‘Header’ Component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
+import Header from "./Header";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <div>
-          Phone Directory
-        </div>
+        <Header />
         <button>Add</button>
         <div>
           <span> Name </span><br />
           <span> Phone </span>
-        </div>
+        </div> 
       </div>
     );
   }
